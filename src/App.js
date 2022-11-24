5ad72ab548d1b78fbb3283f546f4ebcbd6f6a444
Symbat <shimbatoo@gmail.com>
2022-11-24 15:42:42 +0600

    init:start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Categories } from './components/Categories';
+import { HeaderPizza } from './components/HeaderPizza';
+import { PizzaBlock } from './components/PizzaBlock';
+import { Sort } from './components/Sort';
+import './scss/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <HeaderPizza />
+      <div className="content">
+        <div className="container">
+          <div className="content_top">
+            <Categories />
+            <Sort />
+            <h2 className="content__title"> Все Пиццы</h2>
+            <div className="content__items">
+              <PizzaBlock />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
